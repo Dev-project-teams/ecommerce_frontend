d5be9bb57d976a96bf4fc71295a6f5f35e8265ff
Dharshan <dharshanwilson21@gmail.com>
2025-06-21 13:14:59 +0530

    shadcn installed

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
 <>
-DDDD
+<Button>
+  TeST
+  </Button>
+
 </>
   );
 }
