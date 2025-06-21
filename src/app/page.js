4b0cb54506a348bb3ea0896249824e866c5dc709
Dharshan <dharshanwilson21@gmail.com>
2025-06-21 13:20:31 +0530

    next js theme toogler setuped

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
+import { ModeToggle } from "@/components/app-theme-toggler";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-<>
-<Button>
-  TeST
-  </Button>
-
-</>
+    <>
+      <Button>
+        TeST
+      </Button>
+      <ModeToggle />
+    </>
   );
 }
